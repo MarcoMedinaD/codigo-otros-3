36a1aa70565c3c2963eec0167d359a78e339b43e
Marco Medina <chicomrc.mm@gmail.com>
2025-01-24 14:20:45 -0600

    Se realizan modificaciones

--- filtro.js
+++ filtro.js
@@ -8,7 +8,7 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementByName("lista-de-productos") /* Se modifica getElements a getElement */
 const $i = document.querySelector('.input');
 
 for (let i = 0; i < productos.length; i++) {
